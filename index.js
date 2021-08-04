22fd3bd421af7ce6e21647356676eae2944acecc
AdamBasheer <adam.basheer@amalitech.org>
2021-08-04 13:57:07 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Calculate = {
+    factorial(number) {
+    for (let i = number - 1; i >= 1; i--) {
+    if(input === 0) {
+      return 1;
+    }
+    number = number * i; 
+  }
+   
+  return number
+    }
+  }
+  
+  module.exports = Calculate;
+  
+  
